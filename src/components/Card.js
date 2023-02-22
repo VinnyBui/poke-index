5da2e7c480d12b643b5642a58719d4d752964dc0
Vinh B <vinh.bui0101@gmail.com>
2023-02-21 23:24:09 -0800

    change style of pokemon cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,21 +6,21 @@ function Card({ pokemon }) {
         <div className="card w-96 bg-base-100 shadow-xl">
         <figure><img src={pokemon.sprites.front_default} alt="pokemon"/></figure>
         <div className="card-body">
-            <h2 className="card-title">
+            <h2 className="card-title font-extrabold">
             {pokemon.name}
             </h2>
             <div className='flex'>
-                <p>Weight</p>
-                <p>{pokemon.weight}</p>
-                <p>Height</p>
-                <p>{pokemon.height}</p>
-                <p>Ability</p>
-                <p>{pokemon.abilities[0].ability.name}</p>
+                <p className='font-bold'>Weight</p>
+                <p className='font-semibold'>{pokemon.weight}</p>
+                <p className='font-bold'>Height</p>
+                <p className='font-semibold'>{pokemon.height}</p>
+                <p className='font-bold'>Ability</p>
+                <p className='font-semibold'>{pokemon.abilities[0].ability.name}</p>
             </div>
             <div className="card-actions justify-end">
                 {pokemon.types.map(type => {
                     return (
-                        <div className="badge badge-outline">
+                        <div className="badge badge-outline font-semibold">
                             {type.type.name}
                         </div>
                     )
